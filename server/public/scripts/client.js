25840120522740fa78e263760fc3f72d75e67f1a
Lisette Stetson <lisette.stetson@gmail.com>
2022-04-29 19:34:25 -0400

    added click functions for the buttons, and got them performing their purposes successfully

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,26 @@
-console.log( 'js' );
-
 $( document ).ready( onReady );
 
 function onReady(){
-    console.log( 'JQ' );
+    $( '#calculateButton' ).on( 'click', newEquation );
+    $( '#clearButton' ).on( 'click', clearEquation );
+
+}
+
+function clearEquation(){
+    console.log( "in  clearEquation" );
+
+   $( '#numOneInput' ).val( '' );
+   $( '#mathOperatorInput' ).val( '' );
+   $( '#numTwoInput' ).val( '' );
+}
+
+function newEquation(){
+    console.log( "in  newEquation" );
+    let equationInputs = {
+        firstNum: $( '#firstNumInput' ).val(),
+        operator: $( '#mathOperatorInput' ).val(),
+        secondNum: $( '#secondNumInput' ).val(),
+    }
+    console.log( equationInputs, equationInputs.firstNum, equationInputs.operator, equationInputs.secondNum );
+    //send object to server via
 }
