86eb754abfb2e1498fde0ab2fdb2326b86b31b76
Lisette Stetson <lisette.stetson@gmail.com>
2022-04-29 21:40:22 -0400

    added the addition function , got it working in the POST

--- server/modules/calculate.js
+++ server/modules/calculate.js
@@ -1,13 +1,31 @@
 const express = require( 'express' );
 const router = express.Router();
-
+let calculationHistory = []
+let newCalculation = {}
+let solutionDisplay = {}
 //routes
 
 router.post ( '/', (req,res) =>{ //I am not sure if this is correct. Should '/calculator' go here instead?
     console.log('in /calculate/   POST:', req.body); 
     //calculations function here;
+    let newSolution = addition( req.body.firstNum, req.body.secondNum);
+    solutionDisplay.newSolution = newSolution;
+    console.log(solutionDisplay);
+    newCalculation.firstNum = req.body.firstNum;
+    newCalculation.operator = req.body.operator;
+    newCalculation.secondNum = req.body.secondNum;
+    newCalculation.solution = newSolution;
     //push to history array here;
+    calculationHistory.push( newCalculation );
+    console.log( newCalculation );
     res.sendStatus( 200 );
 })
 
 module.exports = router;
+
+//calculation functions
+
+function addition( firstNum, secondNum ){
+    let solution = Number( firstNum ) + Number( secondNum );
+    return solution;
+}
