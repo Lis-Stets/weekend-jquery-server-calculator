25ca9a274ef419d7d42aaa7973323039d29bb973
Lisette Stetson <lisette.stetson@gmail.com>
2022-04-30 15:27:39 -0400

    all four math operations are now functional

--- server/modules/calculate.js
+++ server/modules/calculate.js
@@ -1,20 +1,43 @@
 const express = require( 'express' );
 const router = express.Router();
-let calculationHistory = []
-let newCalculation = {}
-let solutionDisplay = {}
+const calculationHistory = []
+const newCalculation = {}
+const solutionDisplay = {}
 //routes
 
 router.post ( '/', (req,res) =>{ //I am not sure if this is correct. Should '/calculator' go here instead?
     console.log('in /calculate/   POST:', req.body); 
     //calculations function here;
-    let newSolution = addition( req.body.firstNum, req.body.secondNum);
-    solutionDisplay.newSolution = newSolution;
-    console.log(solutionDisplay);
     newCalculation.firstNum = req.body.firstNum;
     newCalculation.operator = req.body.operator;
     newCalculation.secondNum = req.body.secondNum;
+
+    if( req.body.operator === '+' ){
+    let newSolution = addition( req.body.firstNum, req.body.secondNum);
+    solutionDisplay.newSolution = newSolution;
+    console.log(solutionDisplay);
     newCalculation.solution = newSolution;
+    } //end addition if
+    else if( req.body.operator === '-' ){
+        let newSolution = subtraction( req.body.firstNum, req.body.secondNum);
+        solutionDisplay.newSolution = newSolution;
+        console.log(solutionDisplay);
+        newCalculation.solution = newSolution;
+    } // end subtraction else if
+    else if( req.body.operator === '*' ){
+        let newSolution = multiplication( req.body.firstNum, req.body.secondNum);     
+        solutionDisplay.newSolution = newSolution;
+        console.log(solutionDisplay);
+        newCalculation.solution = newSolution;
+    } //end multiplication else if
+    else if( req.body.operator === '/' ){
+        let newSolution = division( req.body.firstNum, req.body.secondNum);
+        solutionDisplay.newSolution = newSolution;
+        console.log(solutionDisplay);
+        newCalculation.solution = newSolution;
+    }//end multiplication else if
+    console.log(solutionDisplay);
+    
     //push to history array here;
     calculationHistory.push( newCalculation );
     console.log( newCalculation );
@@ -27,5 +50,17 @@ module.exports = router;
 
 function addition( firstNum, secondNum ){
     let solution = Number( firstNum ) + Number( secondNum );
+    return solution;
+}
+function subtraction( firstNum, secondNum ){
+    let solution = Number( firstNum ) - Number( secondNum );
+    return solution;
+}
+function multiplication( firstNum, secondNum ){
+    let solution = Number( firstNum ) * Number( secondNum );
+    return solution;
+}
+function division( firstNum, secondNum ){
+    let solution = Number( firstNum ) / Number( secondNum );
     return solution;
 }
